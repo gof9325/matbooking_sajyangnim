323c56024a9cf7af186683e121fd8180c98a6800
황경원 <gof9325@gmail.com>
2022-10-07 21:15:26 +0900

    Initial Commit

---
+++ matbookig_sajyangnim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  matbookig_sajyangnim
+//
+//  Created by 황경원 on 2022/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ matbookig_sajyangnim/matbookig_sajyangnimApp.swift
@@ -0,0 +1,17 @@
+//
+//  matbookig_sajyangnimApp.swift
+//  matbookig_sajyangnim
+//
+//  Created by 황경원 on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct matbookig_sajyangnimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
